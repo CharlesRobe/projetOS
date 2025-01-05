5152a91a28ad9b8d9654a0d7ae9720de0463e2ce
jwillems202415 <j.willems@students.ephec.be>
2025-01-05 13:13:28 +0100

    Création automatique de "circuits_inities.txt" (Solution provisoire)
    Le fichier de "compiler.sh" crée "circuits_inities.txt" lors de la compilation, pour temporairement solutionner le problème adressé à l'issue #8

--- compiler.sh
+++ compiler.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 gcc main.c main_functions.c -o Formula1_OS
+echo "[ OK ] Compile necessary files."
+touch "circuits_inities.txt"
+echo "[ OK ] Create file 'circuits_inities.txt'"
+echo "All done. You may now run the command '$ ./Formula1_OS' to execute the program."
